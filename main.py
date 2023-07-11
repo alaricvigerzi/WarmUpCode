f98b9a7be43f841f5e85b6f96a387100f7cb6ed7
alaricvigerzi <139182228+alaricvigerzi@users.noreply.github.com>
2023-07-11 23:38:07 +0000

    Added code for step 1

---
+++ main.py
@@ -0,0 +1,15 @@
+# Print my name
+print("Alaric")
+
+# Assign my favourite number to a variable
+fav_num = 9
+# Find my favourite number squared
+fav_num_sq = fav_num * fav_num
+
+# Create variable for a count up loop
+count_up = 1
+
+# Loop to count up to my favourite number squared
+while (count_up <= fav_num_sq):
+  print(count_up)
+  count_up = count_up+1
